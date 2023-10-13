4f436ac2dd32ae82d8ecd836521e6f99cca4c0ef
Ane Christine Kanestrøm <146572172+AneKanestrom@users.noreply.github.com>
2023-10-13 12:23:35 +0200

    hamburger nav

--- P3/JS/nav.js
+++ P3/JS/nav.js
@@ -3,11 +3,11 @@ function addNavbar() {
     var navcode = '<nav id="desktop-nav">'+
     '        <div class="logo"> '+
     '            <a href="HomePage.html">'+
-    '                <img src="../images/HjemForPoter-navbar-logo.png" alt="Logo">'+
+    '                <img src="../images/Logo.png" alt="Logo">'+
     '            </a>'+
     '        </div>'+
     '        <div>'+
-    '            <ul class="nav-links trasparent">'+
+    '            <ul class="nav-links">'+
     '                <li> <a href="AboutUs.html">OM OSS</a></li>'+
     '                <li> <a href="Dogs.html">HUNDER</a></li>'+
     '                <li> <a href="Donate.html">DONER</a></li>'+
