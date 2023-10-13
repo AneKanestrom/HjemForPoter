7b0b13ea95a7d56261b048b92f7b8c90521ef46e
Ane Christine Kanestrøm <146572172+AneKanestrom@users.noreply.github.com>
2023-10-13 19:35:53 +0200

    js nav

--- P3/JS/nav.js
+++ P3/JS/nav.js
@@ -1,19 +1,27 @@
 function addNavbar() {
     let navbar = document.getElementById("navbar");
-    var navcode = '<nav id="desktop-nav">'+
+    var navcode = '<nav class="desktop-nav">'+
     '        <div class="logo"> '+
     '            <a href="HomePage.html">'+
     '                <img src="../images/Logo.png" alt="Logo">'+
     '            </a>'+
     '        </div>'+
-    '        <div>'+
+    '        <div >'+
     '            <ul class="nav-links">'+
-    '                <li> <a href="AboutUs.html">OM OSS</a></li>'+
-    '                <li> <a href="Dogs.html">HUNDER</a></li>'+
-    '                <li> <a href="Donate.html">DONER</a></li>'+
+    '                <li onclick="toggle()">'+
+    '                    <div class="hamburger">'+
+    '                        <div class="bar"></div>'+
+    '                        <div class="bar"></div>'+
+    '                        <div class="bar"></div>'+
+    '                    </div>'+
+    '                </li>'+
+    '                <li class="nav-element"> <a href="AboutUs.html">OM OSS</a></li>'+
+    '                <li class="nav-element"> <a href="Dogs.html">HUNDER</a></li>'+
+    '                <li class="nav-element"> <a href="Donate.html">DONER</a></li>'+
     '            </ul>'+
     '        </div>'+
-    '    </nav>';
+    '    </nav>'+
+    '    <script src="../JS/hamburger.js"> </script>';
         
     navbar.innerHTML = navcode;
 }
