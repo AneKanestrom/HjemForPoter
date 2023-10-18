55008c7b5d2b89cb865a814675bc70d43666064d
Guro <gurokri@stud.ntnu.no>
2023-10-18 21:28:45 +0200

    start paa adopsjonsskjema

---
+++ P3/JS/Adoptionform.js
@@ -0,0 +1,42 @@
+let butn = document.getElementById('adopsjonsknapp');
+
+function clickEventhandler(){
+    /* legg til funksjonalitet for å få opp skjema for adopsjon*/
+    
+    //lager et skjema for å ville adoptere en hund 
+    /*var form = document.createElement('form');
+
+    var inputName = document.createElement('input');
+    inputName.type = 'text';
+    inputName.name = 'navn'
+
+    var inputMail = document.createElement('mail');
+    inputMail.type = 'text';
+    inputMail.name = 'mail';
+
+    var inputSøknad = document.createElement('søknad');
+    inputSøknad.type = 'text';
+    inputSøknad.name = 'søknad'
+
+    //lager knapp for å sende inn skjemaet
+    var buttonSend = document.createElement('input');
+    buttonSend.type = 'submit';
+    buttonSend.value = 'Send';
+
+    //legger til elementene i skjemaet
+    form.appendChild(inputName);
+    form.appendChild(inputMail);
+    form.appendChild(inputSøknad);
+    form.appendChild(buttonSend);
+    
+    
+    //legger til skjemaet til DOM (hva er dette??)
+    document.body.appendChild(form);
+    */
+
+    //få opp skjema til nettsiden
+}
+
+
+//knapp for å få opp skjemaet
+butn.addEventListener('click', clickEventhandler);
