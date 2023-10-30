da75cb55df8404ef9f40204aee36028168688138
Guro <gurokri@stud.ntnu.no>
2023-10-30 16:29:08 +0100

    slaatt sammen createDogNPage og createDogBox, forsokt aa fikse indeksering men det er ikke riktig enda

--- P3/JS/createDogNPage.js
+++ P3/JS/createDogNPage.js
@@ -45,7 +45,50 @@ const dogsData = [
 
 
 
-const navnOgOverskrift = document.getElementById("navnOgOverskrift");
+function createDogBox() {
+
+    const dogContainer = document.getElementById("dog-container");
+
+    i = 0;
+    dogsData.forEach((dog) => {
+    
+        const dogBox = document.createElement("div");
+        dogBox.className = "dog-box";
+        //gir de forskjelig id, kan vi bruke dette til å skille mellom hvilke hundeside vi skal linke til?
+        dogBox.id = "dogNo" + i;
+
+        console.log (dogBox.id)
+
+        const img = document.createElement("img");
+        img.src = dog.imageSrc;
+        img.className = "dog-box-bilde";
+        img.alt = dog.name;
+
+        const h3 = document.createElement("h3");
+        h3.textContent = dog.name;
+
+        const genderParagraph = document.createElement("p");
+        genderParagraph.textContent = "Kjønn: " + dog.gender;
+
+        const ageParagraph = document.createElement("p");
+        ageParagraph.textContent = "Alder: " + dog.age;
+
+        const breedParagraph = document.createElement("p");
+        breedParagraph.textContent = "Rase: " + dog.breed;
+
+        dogBox.appendChild(img);
+        dogBox.appendChild(h3);
+        dogBox.appendChild(genderParagraph);
+        dogBox.appendChild(ageParagraph);
+        dogBox.appendChild(breedParagraph);
+
+        
+        dogContainer.appendChild(dogBox);
+
+        i++;
+    });
+}
+
 
 //funksjon for å finne hvilken hun det er snakk om! 
 //tenkt å koble opp mot knapp og actionlistner ?
@@ -67,7 +110,13 @@ const navnOgOverskrift = document.getElementById("navnOgOverskrift");
 
 //i vil nå fortelle oss hvilken hund i lista vi har.
 
+
 function createTopOfDogNPage(indeks) {
+    const navnOgOverskrift = document.getElementById("navnOgOverskrift");
+
+    //navn og overskrfift blir null, aaaaaah
+    console.log(navnOgOverskrift)
+
     //henter ut hunden 
     const dog = dogsData[indeks];
     
@@ -85,9 +134,9 @@ function createTopOfDogNPage(indeks) {
 
 }
 
-const hundeinfo = document.getElementById("hundeinfo");
 
 function createInfo(indeks){ 
+    const hundeinfo = document.getElementById("hundeinfo");
     //henter ut hunden 
     let dog = dogsData[indeks];
 
@@ -98,6 +147,40 @@ function createInfo(indeks){
 
 }
 
-//bytte ut dette slik at vi blir sendt riktig sted når vi klikker på en av knappene (divene) våre, vil bli sendt til riktig huns
-window.onload = createTopOfDogNPage(1);
-window.onload = createInfo(1);
+function showDogNPage(){
+    //bytter path til DogNPage
+
+    //gjør at divene fungerer som knapper med click events!
+    const dogBtn1 = document.getElementById("dogNo0");
+    const dogBtn2 = document.getElementById("dogNo1");
+    const dogBtn3 = document.getElementById("dogNo2");
+
+    dogBtn1.addEventListener('click', function() {
+        localStorage.setItem("dogIndex", 0);
+        createTopOfDogNPage(0);
+        createInfo(0);
+
+        window.location.href = "DogNPage.html"
+    });
+
+    dogBtn2.addEventListener('click', function() {
+        createTopOfDogNPage(1);
+        createInfo(1);
+
+        window.location.href = "DogNPage.html"
+    });
+    dogBtn3.addEventListener('click', function() {
+        createTopOfDogNPage(2);
+        createInfo(2);
+
+        window.location.href = "DogNPage.html"
+    
+    })
+}
+
+//chatGPT sin løsning: funker ikke for oss, kaller funksjoner fra dogs.html
+/*document.addEventListener("DOMContentLoaded", function() {
+    createDogBox();
+    showDogNPage();
+})*/
+//bytte ut dette slik at vi blir sendt riktig sted når vi klikker på en av knappene (divene) våre, vil bli sendt til riktig hund
